1b81eb9ef00b32c31dcaa7f5b537dd640a2caaf9
Anubhav <anubhav_a@me.iitr.ac.in>
2024-09-04 15:18:27 +0530

    Added social to mobile nav

--- portfolio-nextJS/portfolio/components/MobileNav.tsx
+++ portfolio-nextJS/portfolio/components/MobileNav.tsx
@@ -1,48 +1,90 @@
 import { NavButtonMobile } from "./NavButton";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import SocialButtonMobile from "./SocialButtonMobile";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+import { SiCodeforces } from "react-icons/si";
+import { SiLeetcode } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { IconContext } from "react-icons";
+import SocialButton from "./SocialButton";
 
-export interface NavProps{
-    setIsOpen: (a:boolean)=>void;
-    router: AppRouterInstance
+export interface NavProps {
+  setIsOpen: (a: boolean) => void;
+  router: AppRouterInstance;
 }
 
-export default function MobileNav({setIsOpen, router}:NavProps){
-    return <div className="h-screen top-0 right-0 bg-black/80 w-screen flex flex-col items-end absolute fixed">
-    <div className="w-4/5 h-screen flex flex-col bg-slate-900">
-      <div
-        onClick={() => {
-          setIsOpen(false);
-        }}
-        className="w-full flex flex-col items-end p-5"
-      ></div>
-
-      <div className="flex text-xs flex-col items-center justify-around h-2/5">
-        <NavButtonMobile
-          text="About"
-          onClick={() => {}}
-          isActive={true}
-        />
-        <NavButtonMobile
-          text="Experience"
-          onClick={() => {}}
-          isActive={true}
-        />
-        <NavButtonMobile
-          text="Projects"
-          onClick={() => {}}
-          isActive={true}
-        />
+export default function MobileNav({ setIsOpen, router }: NavProps) {
+  return (
+    <div className="h-screen top-0 right-0 bg-black/80 w-screen flex flex-col items-end absolute fixed">
+      <div className="w-4/5 h-screen flex flex-col bg-slate-900">
         <div
           onClick={() => {
-            router.push(
-              "https://drive.google.com/file/d/11G-q2iEenxl5PSMlY5mh7EkSiXKdCAUW/view?usp=sharing"
-            );
+            setIsOpen(false);
           }}
-          className=" mt-5 border border-violet-500 text-violet-500 px-3 py-2 rounded-[4px] cursor-pointer hover:border-transparent transition-all hover:bg-violet-800 hover:text-violet-100 "
-        >
-          Resume
+          className="w-full flex flex-col items-end p-5"
+        ></div>
+
+        <div className="flex text-xs flex-col items-center justify-around h-2/5">
+          <NavButtonMobile text="About" onClick={() => {}} isActive={true} />
+          <NavButtonMobile
+            text="Experience"
+            onClick={() => {}}
+            isActive={true}
+          />
+          <NavButtonMobile text="Projects" onClick={() => {}} isActive={true} />
+          <div
+            onClick={() => {
+              router.push(
+                "https://drive.google.com/file/d/11G-q2iEenxl5PSMlY5mh7EkSiXKdCAUW/view?usp=sharing"
+              );
+            }}
+            className=" mt-5 border border-violet-500 text-violet-500 px-3 py-2 rounded-[4px] cursor-pointer hover:border-transparent transition-all hover:bg-violet-800 hover:text-violet-100 "
+          >
+            Resume
+          </div>
         </div>
+        <div className="flex flex-col items-center h-2/5 justify-center">
+            <div className="text-violet-100/60 text-lg font-semibold">
+              Social
+            </div>
+            <div className="h-3/5 w-4/5 pt-2 border-t-[1px] border-violet-100/10">
+              <div className="flex flex-row w-full justify-around">
+                <SocialButtonMobile
+                  button={<FaInstagram />}
+                  link="https://www.instagram.com/__4nubhav__/"
+                  title={"Instagram"}
+                />
+                <SocialButtonMobile
+                  button={<FaLinkedin />}
+                  link="https://www.linkedin.com/in/anubhav-aaryan-099987201/"
+                  title={"Linkedin"}
+                />
+                <SocialButtonMobile
+                  button={<SiCodeforces />}
+                  link="https://codeforces.com/profile/_watch_this_"
+                  title={"Codeforces"}
+                />
+                <SocialButtonMobile
+                  button={<SiLeetcode />}
+                  link="https://leetcode.com/u/aaryan4nubhav/"
+                  title={"Leetcode"}
+                />
+                <SocialButtonMobile
+                  button={<FaGithub />}
+                  link="https://github.com/dalaixlmao"
+                  title={"Github"}
+                />
+                <SocialButtonMobile
+                  button={<SiGmail />}
+                  link="mailto:aaryan4nubhav@gmail.com"
+                  title={"Gmail"}
+                />
+              </div>
+            </div>
+          </div>
       </div>
     </div>
-  </div>
+  );
 }
---
+++ portfolio-nextJS/portfolio/components/SocialButtonMobile.tsx
@@ -0,0 +1,43 @@
+import { ReactNode, useRef, useState } from "react";
+import { IconType } from "react-icons";
+import { IconContext } from "react-icons";
+import { useRouter } from "next/navigation";
+
+export default function SocialButtonMobile({
+  button,
+  link,
+  title,
+}: {
+  button: ReactNode;
+  link: string;
+  title: string;
+}) {
+  const [hovered, setHovered] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+  const router = useRouter();
+  return (
+    <IconContext.Provider
+      value={{
+        color: hovered ? "#a78bfa" : "white",
+        className: !hovered
+          ? "opacity-50 h-5 w-5 transition-all py-[2px] pr-[4px]"
+          : "opacity-100 h-5 w-5 transition-all py-0 pr-0",
+      }}
+    >
+      <div
+        style={{
+          transitionProperty: "all",
+          transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
+          transitionDuration: "150ms",
+        }}
+        onClick={() => {
+          router.push(link);
+        }}
+        className="flex cursor-pointer flex-row items-center duration-150 transition-all"
+        title={title}
+      >
+        {button}
+      </div>
+    </IconContext.Provider>
+  );
+}
