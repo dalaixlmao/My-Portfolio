7e545345cc83ae9f54c991cac45c8d0d6cc4fec5
Anubhav Aaryan <74524090+dalaixlmao@users.noreply.github.com>
2024-11-26 22:55:25 +0530

    Update resume link

--- store/links.ts
+++ store/links.ts
@@ -1,4 +1,4 @@
-const resume = "https://drive.google.com/file/d/14Vpd1P7cCemwWeJMiH2bwuUxst8Y5f1v/view";
+const resume = "https://bit.ly/3AWg29I";
 const instagram = "https://www.instagram.com/__4nubhav__/";
 const linkedin = "https://www.linkedin.com/in/anubhav-aaryan-099987201/";
 const codeforces = "https://codeforces.com/profile/_watch_this_";
